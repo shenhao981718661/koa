85ee90583965615e613bbd1581a5b1682bfb8b14
yutong <yut@gratone.cn>
2019-04-15 22:47:12 +0800

    住户模块添加编辑和删除功能

--- db/housing/housing.js
+++ db/housing/housing.js
@@ -25,5 +25,37 @@ class Mongodb{
             })
         })
     }
+    edit(obj){
+        return new Promise((resolve,reject) => {
+            housingModal.update({_id: obj._id},{
+              $set: {
+                  name: obj.name,
+                  age: obj.age,
+                  sex: obj.sex,
+                  room: obj.room,
+                  tel: obj.tel,
+                  date: obj.date,
+                  remarks: obj.remarks,
+              }
+          }, (err,res) => {
+              if(err){
+                  reject(err)
+              }
+              resolve(res)
+          })
+      })
+      }
+  
+      //删除
+      remove(obj){
+        return new Promise((resolve,reject) => {
+            housingModal.remove({_id: obj._id}, (err,res) => {
+                if(err){
+                    reject(err)
+                }
+                resolve(res)
+            })
+        })
+    }
 }
 module.exports = new Mongodb()
