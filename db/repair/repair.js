9ddd712a79b2249c8f394a27fa81508e11997098
yutong <yut@gratone.cn>
2019-04-15 23:34:40 +0800

    维修模块新增编辑和删除功能

--- db/repair/repair.js
+++ db/repair/repair.js
@@ -25,5 +25,34 @@ class Mongodb{
             })
         })
     }
+    edit(obj){
+        return new Promise((resolve,reject) => {
+            repairModal.update({_id: obj._id},{
+              $set: {
+                  name: obj.name,
+                  content: obj.content,
+                  money: obj.money,
+                  date: obj.date,
+              }
+          }, (err,res) => {
+              if(err){
+                  reject(err)
+              }
+              resolve(res)
+          })
+      })
+      }
+  
+      //删除
+      remove(obj){
+        return new Promise((resolve,reject) => {
+            repairModal.remove({_id: obj._id}, (err,res) => {
+                if(err){
+                    reject(err)
+                }
+                resolve(res)
+            })
+        })
+    }
 }
 module.exports = new Mongodb()
