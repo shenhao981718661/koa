779ff73a9be5992fa09f9a3382e7486e7d1a8616
yutong <yut@gratone.cn>
2019-04-15 22:19:39 +0800

    业主模块增加修改和删除功能

--- routers/complaint.js
+++ routers/complaint.js
@@ -8,4 +8,10 @@ const routers = router
     .post('/add', async (ctx) => {
         ctx.response.body = await db.add(ctx.request.body)
     })
+    .post('/edit', async (ctx) => {
+        ctx.response.body = await db.edit(ctx.request.body)
+    })
+    .post('/remove', async (ctx) => {
+        ctx.response.body = await db.remove(ctx.request.body)
+    })
 module.exports = routers
