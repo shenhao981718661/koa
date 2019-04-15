b5e2237428fd5a4debd238055e747be8f9be1202
yutong <yut@gratone.cn>
2019-04-15 23:12:02 +0800

    投诉模块新增编辑和删除功能

--- db/complaint/complaint.js
+++ db/complaint/complaint.js
@@ -25,5 +25,34 @@ class Mongodb{
             })
         })
     }
+    edit(obj){
+        return new Promise((resolve,reject) => {
+            complaintModal.update({_id: obj._id},{
+              $set: {
+                  name: obj.name,
+                  content: obj.content,
+                  date: obj.date,
+                  result: obj.result,
+              }
+          }, (err,res) => {
+              if(err){
+                  reject(err)
+              }
+              resolve(res)
+          })
+      })
+      }
+  
+      //删除
+      remove(obj){
+        return new Promise((resolve,reject) => {
+            complaintModal.remove({_id: obj._id}, (err,res) => {
+                if(err){
+                    reject(err)
+                }
+                resolve(res)
+            })
+        })
+    }
 }
 module.exports = new Mongodb()
